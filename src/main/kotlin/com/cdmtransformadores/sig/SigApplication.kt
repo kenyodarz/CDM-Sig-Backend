5ca156684f2815200c8c1e4b51f5a84411be581f
Kenyodarz <Kenyodarz@gmail.com>
2020-09-03 08:31:19 -0500

    Initial commit

---
+++ src/main/kotlin/com/cdmtransformadores/sig/SigApplication.kt
@@ -0,0 +1,11 @@
+package com.cdmtransformadores.sig
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SigApplication
+
+fun main(args: Array<String>) {
+	runApplication<SigApplication>(*args)
+}
---
+++ src/test/kotlin/com/cdmtransformadores/sig/SigApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cdmtransformadores.sig
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SigApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
